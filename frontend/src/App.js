b1286967fec88f69281784886471c98bda877160
BenedictBCJJ <bennyyxz@gmail.com>
2020-05-29 10:55:54 +0800

    Added logo to the homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
-import {Route} from 'react-router-dom';
-import NavBar from './NavBar/NavBar';
-import Question from './Question/Question';
-import Questions from './Questions/Questions';
-import Callback from './Callback';
-import SecuredRoute from './SecuredRoute/SecuredRoute';
-import NewQuestion from './NewQuestion/NewQuestion';
+import React, { Component } from "react";
+import NavBar from "./NavBar/NavBar";
+import Logo from "./Logo";
+// import { Route } from "react-router-dom";
+
+// import Question from "./Question/Question";
+// import Questions from "./Questions/Questions";
+// import Callback from "./Callback";
+// import SecuredRoute from "./SecuredRoute/SecuredRoute";
+// import NewQuestion from "./NewQuestion/NewQuestion";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <NavBar/>
-        <Route exact path='/' component={Questions}/>
+        <NavBar />
+        <Logo name="FrontPageLogo" link="index.html" />
+        {/* <SearchBar /> */}
+        {/* <Route exact path='/' component={Questions}/>
         <Route exact path='/question/:questionId' component={Question}/>
         <Route exact path='/callback' component={Callback}/>
-        <SecuredRoute path='/new-question' component = {NewQuestion}/>
+        <SecuredRoute path='/new-question' component = {NewQuestion}/> */}
       </div>
     );
   }
