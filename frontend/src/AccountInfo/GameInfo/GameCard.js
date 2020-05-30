7f14b7d38c0cf1c70d4dfe45a83cfedb68aee6c1
BenedictBCJJ <bennyyxz@gmail.com>
2020-05-30 14:11:32 +0800

    Removed navbar css

--- frontend/src/AccountInfo/GameInfo/GameCard.js
+++ frontend/src/AccountInfo/GameInfo/GameCard.js
@@ -1,24 +1,36 @@
-import React, {Component} from 'react';
-import CardStatistics from '../CardStatistics';
+import React, { Component, useState } from "react";
+import CardStatistics from "../CardStatistics";
 
 function MovieCard() {
-	return(
-		<div class="card" style={{width: '80rem'}}>
+  return (
+    <div class="card" style={{ width: "80rem" }}>
       <div class="card-body">
         <h5 class="card-title">The Flash</h5>
         <div class="custom-control custom-checkbox">
-          <input type="checkbox" class="custom-control-input" id="customCheck1" checked=""></input>
-          <label class="custom-control-label" for="customCheck1">Completed</label> 
+          <input
+            type="checkbox"
+            class="custom-control-input"
+            id="customCheck1"
+            checked=""
+            value=""
+          ></input>
+          <label class="custom-control-label" for="customCheck1">
+            Completed
+          </label>
         </div>
         <p class="card-text">Last Watched on 1/1/2020</p>
-				<div>
-					<a href="#" class="card-link" style={{float:'left'}}>Continue where you left off</a>
-        	<a href="#" class="card-link" style={{float:'left'}}>Read Reviews</a>
-					<CardStatistics style={{float:'left'}}></CardStatistics>
-				</div>
+        <div>
+          <a href="#" class="card-link" style={{ float: "left" }}>
+            Continue where you left off
+          </a>
+          <a href="#" class="card-link" style={{ float: "left" }}>
+            Read Reviews
+          </a>
+          <CardStatistics style={{ float: "left" }}></CardStatistics>
+        </div>
       </div>
     </div>
-	);
+  );
 }
 
 export default MovieCard;
